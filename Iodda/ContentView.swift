2c92cb999826fb106b28472897331400a60b5b3c
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-06-27 18:13:05 +0300

    Initial Commit

---
+++ Iodda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Iodda
+//
+//  Created by Vlad on 27/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Iodda/IoddaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoddaApp.swift
+//  Iodda
+//
+//  Created by Vlad on 27/6/25.
+//
+
+import SwiftUI
+
+@main
+struct IoddaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
